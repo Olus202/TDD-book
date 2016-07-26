93e993c221aabd90bd6345f6a1b425d82bbd1e91
Olus202 <aleksandrabacik@gmail.com>
2016-07-26 21:31:26 +0200

    Wprowadzenie drugiego użytkownika

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -43,9 +43,13 @@ class NewVisitorTest(LiveServerTestCase):
         # "1. Buy the peacock feathers" as the element of the list.
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy the peacock feathers', [row.text for row in rows])
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+
+#        table = self.browser.find_element_by_id('id_list_table')
+#        rows = table.find_elements_by_tag_name('tr')
+#        self.assertIn('1: Buy the peacock feathers', [row.text for row in rows])
 
         # It's still the text box on the page.
         # Typing "Use the peacock feathers to make a bait" as a second element.
@@ -54,10 +58,42 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page has been updated again, now it's two elements on the list.
-        self.check_for_row_in_list_table('1: Buy the peacock feathers')
-        self.check_for_row_in_list_table('2: Use the peacock feathers to make a bait')
 
-        # Is it remember on the site? It generated special url with the comment.
+        self.check_for_row_in_list_table('2: Use the peacock feathers to make a bait')
+        self.check_for_row_in_list_table('1: Buy the peacock feathers')
+
+        # Now new user Frank wants to use a website.
+
+        ## We will use a new session of the browser to be sure
+        ## that any information from Edith list, will not visible in Frank list (e.g. by cookies).
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Frank run the website.
+        # He did not find any elements from Edith's list.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy the peacock feathers', page_text)
+        self.assertNotIn('to make a bait', page_text)
+
+        # Frank create a new list by entering a new element.
+        # He's list is less interested than Edith's list.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy a milk.')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Frank hav also a unique url for he's list.
+        frank_list_url = self.browser.current_url
+        self.assertRegex(frank_list_url, '/lists/.+')
+        self.assertNotEqual(frank_list_url, edith_list_url)
+
+        # It's still any element of Edith's list.
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy the peacock feathers', page_text)
+        self.assertIn('Buy a milk.', page_text)
+
+
+        ## Control error
         self.fail('End of test!')
 
         # Go to the special URL - there it a list of things to do.
